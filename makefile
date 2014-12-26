08770803b5db942c3b5c034ff15927aa9e611dbb
Paul Greenberg <paul@greenberg.pro>
2014-12-26 16:16:02 -0500

    improvements

--- makefile
+++ makefile
@@ -1,12 +1,34 @@
-# Prerequisites:
-#  - Set DEV_BACKUP_DIR environment variable via /etc/profile.d/developer.sh   
-#    
-#    sudo bash -c "echo '# User Development Environment' > /etc/profile.d/developer.sh"
-#    sudo bash -c "echo 'DEV_BACKUP_DIR=~/dev/backup' >> /etc/profile.d/developer.sh"
-#    sudo bash -c "echo 'export DEV_BACKUP_DIR' >> /etc/profile.d/developer.sh"
-#    sudo bash -c "chmod 644 /etc/profile.d/developer.sh"
+#######################################################################################################
 #
-#  - Set GIT_USER local variable
+#   PyNetConf - IETF Network Configuration Protocol (NETCONF) Client Library
+#   Copyright (C) 2014 Paul Greenberg <paul@greenberg.pro>
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#
+#   Prerequisites:
+#
+#     - Set DEV_BACKUP_DIR environment variable via /etc/profile.d/developer.sh   
+#          
+#       sudo bash -c "echo '# User Development Environment' > /etc/profile.d/developer.sh"
+#       sudo bash -c "echo 'DEV_BACKUP_DIR=~/dev/backup' >> /etc/profile.d/developer.sh"
+#       sudo bash -c "echo 'export DEV_BACKUP_DIR' >> /etc/profile.d/developer.sh"
+#       sudo bash -c "chmod 644 /etc/profile.d/developer.sh"
+#
+#     - Set GIT_USER local variable
+#
+#######################################################################################################
 
 APP_NAME=PyNetConf
 APP_VERSION=1.0
@@ -36,7 +58,7 @@ clean:
 backup:
 	@echo "Backup ..."
 	@mkdir -p ${DEV_BACKUP_DIR}
-	@cd ..; tar -cvzf ${DEV_BACKUP_FILE} --exclude='*/.git*' ${APP_NAME}; cd ${APP_NAME}
+	@cd ..; tar -cvzf ${DEV_BACKUP_FILE} --exclude='*/.git*' --exclude='*__pycache__*' ${APP_NAME}; cd ${APP_NAME}
 	@echo "Completed! Run \"tar -ztvf ${DEV_BACKUP_FILE}\" to verify ..."
 
 git:
--- tests/test.py
+++ tests/test.py
@@ -22,17 +22,17 @@ import traceback;
 try:
     from pynetconf import NetConfSession;
 except Exception as err:
-    print('%-10s %s' % ('import():', 'Error: ' + str(err)));
-    for i in str(traceback.format_exc()).splitlines():
-        print('%-10s %s' % ('import():', i));
+    for e in err.args:
+        print('%-10s >>   %s: %s' % ('global()', str(type(err).__name__), str(e)));
     sys.exit(1);
 
 def main():    
     func = 'main()';
     descr = 'PyNetConf - IETF Network Configuration Protocol Client Library\n\n';
     descr += 'examples:\n';
-    descr += '  python3 tests/test.py -ho router -u admin -p Y2lzY28K -l 10\n';
+    descr += '  python3 tests/test.py -ho router -u admin -p Y2lzY28K -l 5\n';
     descr += '    [note: "Y2lzY28K" is base64-encoded "cisco"]\n';
+    descr += '  python3 tests/test.py -ho router -u admin -k ~/.ssh/id_rsa -l 5\n';
     descr += '  python3 tests/test.py --help';
     epil = 'documentation:\n  https://github.com/greenpau/PyNetConf\n \n';
     parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter,description=descr, epilog=epil);
@@ -43,21 +43,51 @@ def main():
     auth_group.add_argument('-u', '--user', dest='iuser', metavar='USERNAME', type=str, required=True, help='username');
     credgroup = auth_group.add_mutually_exclusive_group(required=True);
     credgroup.add_argument('-p', '--pass', dest='ipass', metavar='PASSWORD', type=str, help='base64-encoded password');
-    credgroup.add_argument('-k', '--key', dest='ikey', metavar='SSH_KEY_FILE_PATH', type=argparse.FileType('r'), help='SSH private key file path');
-    auth_group.add_argument('-c', '--check-fingerprint', dest='FINGERPRINT_CHECK', action='store_true', help='enable SSH fingerprint check');
-
-    parser.add_argument('-l', '--log', dest='ilog', metavar='LEVEL', type=int, default=0, help='log level (default: 0)');
+    credgroup.add_argument('-k', '--key', dest='ipass', metavar='SSH_KEY_FILE_PATH', type=argparse.FileType('r'), help='SSH private key file path');
+    auth_group.add_argument('--check-fingerprint', dest='ifc', action='store_true', help='enable SSH fingerprint check');
+    parser.add_argument('-l', '--log-level', dest='ilog', metavar='LOG_LEVEL', type=int, default=0, choices=range(1, 6), help='log level (default: 0)');
     args = parser.parse_args();
 
-    if args.ilog < 20:
-        print('%-10s %s = %d' % (func + ':', 'log level', args.ilog));
-
     try:
-        nc = NetConfSession();
+        nc = NetConfSession(args.ihost, args.iuser, args.ipass, args.iport, args.ifc, args.ilog);
+
+        nc.connect();
+        if nc.error:
+            raise RuntimeError('connectivity issue');
+
+        ''' display client and server capabilities '''
+        nc.show('client-capabilities');    # alternatively, pprint.pprint(nc.capabilities['client']);  
+        nc.show('server-capabilities');    # alternatively, pprint.pprint(nc.capabilities['server']);  
+
+        ''' 
+
+            +---------+-----------+-------------+--------------+--------------+-------------+---------+
+            | Command | Cisco IOS | Cisco CatOS | Cisco IOS XE | Cisco IOS XR | Cisco NX-OS | Juniper |
+            +---------+-----------+-------------+--------------+--------------+-------------+---------+
+            | show run                                                                                |
+            | show version                                                                            |
+            | show interfaces                                                                         |
+            +-----------------------------------------------------------------------------------------+
+
+        '''
+
+        cmds = ['show run', 'show version', 'show interfaces'];
+        for c in cmds:
+            nc.cmd(c);
+            if nc.resp:
+                nc.show('response');
+            if nc.error:
+                raise RuntimeError('command request-response issues');
+            if len(nc.log) > 0:
+                nc.show('log');
+        nc.close();
+
     except Exception as err:
-        print('%-10s %s' % (func + ':', 'Error: ' + str(err)));
-        for i in str(traceback.format_exc()).splitlines():
-            print('%-10s %s' % ('import():', i));
+        for e in err.args:
+            print('%-10s >>   %s: %s' % (func, str(type(err).__name__), str(e)));
+        if args.ilog == 5:
+            for i in str(traceback.format_exc()).splitlines():
+                print('%-10s >>   %s' % (func, i));
         return;
 
 if __name__ == '__main__':
